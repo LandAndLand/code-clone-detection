33d2e147aceff216ef317c9d3e621e919b94e951
Prashant Baisla <prashantbaisla@gmail.com>
2016-03-11 13:35:05 +0530

    Implement build_paths_c except for If/TernaryOp

--- code_clone_detection/ASTPath.py
+++ code_clone_detection/ASTPath.py
@@ -44,5 +44,24 @@ class ASTPath:
 
   @staticmethod
   def build_paths_c(node, paths, path_so_far):
-    pass
+    path_so_far += node.__class__.__name__ + "-"
+
+    try:
+      for child_node in node.ext:
+        ASTPath.build_paths_c(child_node, paths, path_so_far)
+    except AttributeError:
+      try:
+        for child_node in node.body.block_items:
+          ASTPath.build_paths_c(child_node, paths, path_so_far)
+      except AttributeError:
+        try:
+          for child_node in node.stmt.block_items:
+            ASTPath.build_paths_c(child_node, paths, path_so_far)
+        except AttributeError:
+          try:
+            if path_so_far.endswith("If-"):
+              print(node.iftrue.children) # Make it work
+          except AttributeError:
+            if path_so_far:
+              paths.append(path_so_far)
 
